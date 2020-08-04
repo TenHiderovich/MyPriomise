39349067e365f56d2c07b3e44a725c8595a4c165
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-08-04 09:06:26 +0800

    added then method and test for it

--- promise/promise.js
+++ promise/promise.js
@@ -1,5 +1,25 @@
 class MyPromise {
+  constructor(executor) {
+    this.queue = []
+    executor.call(null, this.onResolve.bind(this), this.onReject.bind(this))
+  }
 
+  onResolve(data) {
+    this.queue.forEach(callback => {
+      data = callback(data)
+    })
+  }
+
+  onReject() {}
+
+  then(fn) {
+    this.queue.push(fn)
+    return this
+  }
+
+  catch(fn) {}
+
+  finally(fn) {}
 }
 
 module.exports = MyPromise;
--- promise/promise.test.js
+++ promise/promise.test.js
@@ -2,9 +2,34 @@ const MyPromise = require('./promise')
 
 describe('My Promise', () => {
 
+  let promise
+  let executorSpy
+
+  const successResult = 42
+
+  beforeEach(() => {
+    executorSpy = jest.fn((r) => setTimeout(() => r(successResult), 150))
+    promise = new MyPromise(executorSpy)
+  })
+
   test('should exists and to be typeof function', () => {
     expect(MyPromise).toBeDefined()
     expect(typeof MyPromise).toBe('function')
+  })
+
+  test('instance should has methods then, catch and finally', () => {
+    expect(promise.then).toBeDefined()
+    expect(promise.catch).toBeDefined()
+    expect(promise.finally).not.toBeUndefined() // в целях изучения, то же что и два предыдущих
+  })
+
+  test('should call executor function', () => {
+    expect(executorSpy).toHaveBeenCalled()
+  })
+
+  test('should get data in then block and chain them', async () => {
+    const result = await promise.then(num => num).then(num => num * 2)
+    expect(result).toBe(successResult * 2)
   })
 
 });
